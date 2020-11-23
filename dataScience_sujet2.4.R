23556f533e3f614ebb0caf59d14a44738013c77c
florine <florinepltg64@gmail>
2020-11-23 14:19:41 +0100

    descriptive analysis

--- dataScience_sujet2.4.R
+++ dataScience_sujet2.4.R
@@ -3,3 +3,13 @@
 # Created by: Florine | Timi | Axel
 # Created on: 23/11/2020
 
+## Add librairy to read dataframe
+library("readxl")
+## Reading dataframe
+df <- read_excel("BDD brute Confinement.xlsx", col_names = TRUE)
+## Delete the first unused column
+df = df[, -1]
+## Get the stucture of dataframe
+str(dataframe2roueBrute)
+## Get the summary of dataframe
+summary(dataframe2roueBrute)
