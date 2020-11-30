cef0e4f70b730d3756896bac7a0e3c219327b332
florine <florinepltg64@gmail>
2020-11-30 09:33:01 +0100

    descriptive analysis with csv file

--- dataScience_sujet2.4.R
+++ dataScience_sujet2.4.R
@@ -3,13 +3,11 @@
 # Created by: Florine | Timi | Axel
 # Created on: 23/11/2020
 
-## Add librairy to read dataframe
-library("readxl")
-## Reading dataframe
-df <- read_excel("BDD brute Confinement.xlsx", col_names = TRUE)
-## Delete the first unused column
-df = df[, -1]
+
+##### test with CSV
+
+dfCsv <- read.csv2("BddBruteConfinement.csv", header = TRUE)
 ## Get the stucture of dataframe
-str(dataframe2roueBrute)
+str(dfCsv)
 ## Get the summary of dataframe
-summary(dataframe2roueBrute)
+summary(dfCsv)
